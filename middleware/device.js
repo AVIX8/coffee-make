9442e1558f0e477ea08a6977f610a35ae7c63ced
ToxicCreator <aleks7000bol@gmail.com>
2021-07-18 13:06:21 +0300

    Редирект на главную с мобильной версии бизнеса

--- middleware/device.js
+++ middleware/device.js
@@ -1,12 +1,9 @@
 export default function ({ $device, redirect, route }) {
   const paths = route.path.match(/[^/]+/g)
-  let changed = true
-  if ($device.isMobile && paths[0] !== 'm') paths.unshift('m')
-  else if (!$device.isMobile && paths[0] === 'm') paths.shift()
-  else changed = false
+  if ($device.isMobile && paths[0] === 'business') {
+    paths.length = 0
+  } else return
 
-  if (changed) {
-    const newPath = '/' + paths.join('/')
-    redirect(newPath)
-  }
+  const newPath = '/' + paths.join('/')
+  redirect(newPath)
 }
